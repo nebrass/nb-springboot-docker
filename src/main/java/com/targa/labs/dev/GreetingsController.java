183b13d34209b1261b2f170c470dd83b3ef2459b
Nebrass <lnibrass@gmail.com>
2016-07-08 21:53:26 +0200

    Adding Demo of SpringBootPlugin autocomplete on application.properties file.

--- src/main/java/com/targa/labs/dev/GreetingsController.java
+++ src/main/java/com/targa/labs/dev/GreetingsController.java
@@ -1,6 +1,7 @@
 package com.targa.labs.dev;
 
 import java.time.LocalTime;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsController {
 
+    @Value("${greetings.separator}")
+    private String separator;
+
     @RequestMapping(path = "/time", method = GET)
     public String sayHelloWorld() {
-        return "Hello World @ " + LocalTime.now();
+        return String.format("Hello World %s %s", separator, LocalTime.now());
     }
 
 }
